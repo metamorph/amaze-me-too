d02bb01f178841866da51bdea81ce472fac6249d
Anders Engström <anders.engstrom@hrmsoftware.se>
2015-09-10 23:01:22 +0200

    Make enqueing work!
    Nice and clean so far!

--- src/amaze_2/core.clj
+++ src/amaze_2/core.clj
@@ -1,18 +1,30 @@
 (ns amaze-2.core
   (:require [quil.core :as q]
             [quil.middleware :as m]
-            [clojure.core.async :as async :refer [chan]]))
+            [clojure.core.async :as a :refer [<! >! <!! timeout chan alt! go]]))
 
-(defn update-state [{maze :maze queue :queue :as state}] state)
-(defn draw-state [{maze :maze queue :queue :as state}]
+(defn next-maze
+  "Take the next maze in the queue"
+  [queue timeout]
+  (first (a/alts!! [queue (a/timeout timeout)])))
+
+(defn update-state
+  "Put the next maze on queue in the maze-slot"
+  [state]
+  (if-let [maze (next-maze (:queue state) 1)] ;; Only update the maze-slot if there is a new entry on queue.
+    (assoc state :maze maze)
+    state))
+
+(defn draw-state [{maze :maze}]
   ; Clear the sketch by filling it with light-grey color.
-  (q/background 240))
+  (q/background 80)
+  (q/text (str maze) 100 100 200 200)) ;; Dummy - write a String.
 
 (defn maze-canvas [queue]
     (q/sketch
     :title "Lost?"
     :size [500 500]
-    :setup #({:maze nil :queue queue})
+    :setup (fn [] {:maze nil :queue queue})
     :update update-state
     :draw draw-state
     ;; :on-close #(async/close! queue)
@@ -34,4 +46,3 @@
 ;; Try another way of sending the maze to the canvas.
 ;; The initial state will be created with a channel that will be used to poll
 ;; mazes to generate.
-
