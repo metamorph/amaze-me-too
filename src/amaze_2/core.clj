b6d58ba01c8f748f6fd2d37b671a62dfce10c0d0
Anders Engström <anders.engstrom@hrmsoftware.se>
2015-09-13 22:02:48 +0200

    Hack hack

--- src/amaze_2/core.clj
+++ src/amaze_2/core.clj
@@ -3,6 +3,8 @@
             [quil.middleware :as m]
             [clojure.core.async :as a :refer [<! >! <!! timeout chan alt! go]]))
 
+(def ^:dynamic *cell-scale* 0.8)
+
 (defn next-maze
   "Take the next maze in the queue"
   [queue timeout]
@@ -29,15 +31,25 @@
         diff-x (/ (- w new-w) 2)
         diff-y (/ (- h new-h) 2)]
     [(+ x diff-x) (+ y diff-y) new-w new-h]))
-
+(defn get-maze-cell
+  "Get the value of a cell in a maze"
+  [maze coord]
+  (get-in maze [:cells coord]))
+(defn get-fill-color [cell]
+  (if (:current cell)
+    [0 200 0]
+    (if (:visited cell)
+      [100 100 200]
+      [255 255 255])))
 (defn draw-state [{maze :maze}]
   ; Clear the sketch by filling it with light-grey color.
   (q/background 80)
-  (let [to-rect #(scale-rect 0.8 (cell-coord-to-rect
+  (let [to-rect #(scale-rect *cell-scale* (cell-coord-to-rect
                                   [(:width maze) (:height maze)]
                                   [(q/width) (q/height)] %))]
     (doseq [coord (keys (:cells maze))]
-      (apply q/rect (to-rect coord)))))
+      (q/with-fill (get-fill-color (get-maze-cell maze coord))
+        (apply q/rect (to-rect coord))))))
 
 
 
@@ -59,9 +71,22 @@
   (let [maze {:width width :height height}
         coords (for [x (range 0 width), y (range 0 height)] [x y])]
     (assoc maze :cells (reduce #(assoc %1 %2 {}) {} coords))))
-(defn get-maze-cell
-  "Get the value of a cell in a maze"
-  [maze coord]
-  (get-in maze [:cells coord]))
 
 ;; Sample usage:
+
+;; (def q (a/chan 10))
+;; (defn enqueue [maze queue]
+;;   (a/>!! queue maze)
+;;   maze)
+
+;; (maze-canvas q)
+
+
+;; (-> (create-maze 5 5)
+;;     (enqueue q)
+;;     (assoc-in [:cells [3 3]] {:visited true})
+;;     (enqueue q)
+;;     (assoc-in [:cells [3 2]] {:visited true})
+;;     (enqueue q)
+;;     (assoc-in [:cells [3 1]] {:current true})
+;;     (enqueue q))
